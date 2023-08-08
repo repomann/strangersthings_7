278ae2764c66942b1c9a03caa6f1b14066fcba2b
marveltaylor <marveltaylor85@gmail.com>
2023-08-08 16:45:34 -0700

    imported COHORTNAME from API

--- src/Components/PostForm.jsx
+++ src/Components/PostForm.jsx
@@ -1,6 +1,13 @@
+import COHORTNAME from "../API";
 import { useState } from "react";
 
 export default function PostForm () {
+
+    const fetchPosts = async () => {
+        try {
+            const response = await fetch(`https://strangers-things.herokuapp.com/api/${COHORTNAME}/posts`)
+        }
+    }
 
     return (
         <>
