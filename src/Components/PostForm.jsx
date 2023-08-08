2e774263cf82aa4107fb02c032e1cc5ffd3d47d5
marveltaylor <marveltaylor85@gmail.com>
2023-08-08 16:49:25 -0700

    starter code for PostForm

--- src/Components/PostForm.jsx
+++ src/Components/PostForm.jsx
@@ -6,6 +6,11 @@ export default function PostForm () {
     const fetchPosts = async () => {
         try {
             const response = await fetch(`https://strangers-things.herokuapp.com/api/${COHORTNAME}/posts`)
+            const result = await response.json();
+            console.log(result);
+            return result
+        } catch (err) {
+            console.error("Oops! Try again!");
         }
     }
 
