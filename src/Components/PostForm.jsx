3f649545ca409321728ea97139ce5dcf13d2c4fa
marveltaylor <marveltaylor85@gmail.com>
2023-08-08 18:40:49 -0700

    testing handleSubmit function and adding checkbox code

--- src/Components/PostForm.jsx
+++ src/Components/PostForm.jsx
@@ -4,10 +4,11 @@ import { useState } from "react";
 export default function PostForm () {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
-    const [price, setPrice] = useState();
+    const [price, setPrice] = useState("");
     const [location, setLocation] =useState("");
 
-    const fetchPosts = async () => {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
         try {
             const response = await fetch(`https://strangers-things.herokuapp.com/api/${COHORTNAME}/posts`, {
                 method: "POST",
@@ -15,26 +16,20 @@ export default function PostForm () {
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify({
-                    id: id,
-                    author: author,
-                    description: description,
-                    location: location,
-                    message: message,
-                    price: price,
                     title: title,
-                    willDeliver: "true",
-                    active: "true",
-                    createdAt: "post inserted into database",
-                    updatedAt: "post updated",
-                })
-            })
+                    description: description,
+                    price: price,
+                    location: location,
+                    willDeliver: true,
+                }),
+            });
             const result = await response.json();
             console.log(result);
             setTitle("");
             setDescription("");
             setPrice();
             setLocation("");
-            return result
+            // return result
             // if (result.error) throw result.error;
         } catch (err) {
             console.error("Oops! Something went wrong. Try again!", err);
@@ -43,7 +38,7 @@ export default function PostForm () {
 
     return (
         <>
-        <form>
+        <form onSubmit={handleSubmit}>
             <h2>Add a New Post</h2>
             <label>Title: {""}
                 <input value={title} onChange={(e) => {setTitle(e.target.value);
@@ -70,9 +65,10 @@ export default function PostForm () {
             </label>
 
             <div>
-                <select>
-                    {/* need checkbox for willDeliver here */}
-                </select>
+                {/* <label>Willing to Deliver?:{""}
+                    <input type="checkbox" checked={true} onChange={(e) => {handle the checkbox change here</label>}}>
+                    </input>
+                </label> */}
             </div>
 
             <button className="create" type="submit">Create</button>
