1d4df4cea3d485672df1d34d6008a334c337483c
sabri4 <sabali8@hotmail.com>
2023-08-10 21:28:18 -0600

    updated register

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 
-function Register() {
-  return (
-    <div>Register</div>
-  )
+const Register = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+
+    const formData = new formData(form);
+    const formJson = Object.fromEntries(formData.entries());
+
+    console.log(formJson)
+  }
+  return <form method="post" onSubmit={handleSubmit}>
+    <h3>Register:</h3>
+
+    <label>
+      Username:
+      <input Name="username" defaultValue={""} />
+    </label>
+
+    <hr />
+
+    <label>
+      Password:{" "}
+      <input type="password" name="mypassword" value={'known password'} />
+    </label>
+    
+    <label>
+      Confirm Password:{" "}
+      <input type="confirmpassword" name="confirmedpassword" value={'confirmedpassword'} />
+    </label>
+
+
+  </form>
+ 
 }
 
 export default Register
