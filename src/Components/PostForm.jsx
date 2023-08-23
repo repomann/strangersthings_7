eaa708e0c722687d093e0cb02f775d3df8e80258
marveltaylor <marveltaylor85@gmail.com>
2023-08-23 13:28:24 -0700

    added seller to handleSubmit function

--- src/Components/PostForm.jsx
+++ src/Components/PostForm.jsx
@@ -25,6 +25,7 @@ export default function PostForm () {
                     description: description,
                     price: price,
                     location: location,
+                    seller: seller,
                     willDeliver: checkbox,
                 }),
             });
