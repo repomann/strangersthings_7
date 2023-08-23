b3659279738a0f4332f270886c2ad612296e6f44
repomann <rheimann87@gmail.com>
2023-08-22 21:06:07 -0700

    deleting change to index.jsx to fix merge conflict

--- src/API/index.js
+++ src/API/index.js
@@ -1,5 +1,5 @@
 const COHORTNAME = '2302-ACC-CT-WEB-PT-A'
-export const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORTNAME}/`
+const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORTNAME}/`
 const API = `${BASE_URL}/${COHORTNAME}`
 
 async function apiCall () {
